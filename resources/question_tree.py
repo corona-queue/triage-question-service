aa97453040072f7dfb5fdaeca13e595e919f2662
Tobias Nack <tobiasnack13@googlemail.com>
2020-03-21 13:07:32 +0100

    Add basic structure for mapping of questions

--- resources/question_tree.py
+++ resources/question_tree.py
@@ -1,6 +1,12 @@
+import json
 from flask_restful import Resource
 
 
 class QuestionTreeResource(Resource):
     def get(self):
-        return {'hello': 'world'}
+        question_json = {}
+        with open('files/questions.json') as json_file:
+            question_json = json.load(json_file)
+        # TODO: Build Mapping from keys on concrete texts
+        print(question_json)
+        return question_json
