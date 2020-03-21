34f1f91ef01b7ce8f7c8d11d553114f2ab92c98e
Tobias Nack <tobiasnack13@googlemail.com>
2020-03-21 13:41:08 +0100

    Add bare bones parsing for question_tree endpoing

--- resources/question_tree.py
+++ resources/question_tree.py
@@ -1,15 +1,33 @@
 import json
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
+from utils.english_text_mapping import english_text_map
+from utils.german_text_mapping import german_text_map
+
+
+text_maps = {"eng": english_text_map, "ger": german_text_map}
 
 
 class QuestionTreeResource(Resource):
     def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('lang', type=str)
+
+        args = parser.parse_args()
+
+        active_text_map = text_maps[args["lang"]]
+
         question_json = {}
         with open('files/questions.json') as json_file:
             question_json = json.load(json_file)
-        # TODO: Build Mapping from keys on concrete texts
-        answer_json = {}
-        with open('files/answers.json') as json_file:
-            answer_json = json.load(json_file)
-        question_json["id"] = 
+
+        # Translate text for answers
+        option_results = []
+        for option in question_json["options"]:
+            option_results.append(active_text_map[option])
+        question_json["options"] = option_results
+
+        # Translate text for question
+        text_id = question_json["text"]
+        text_id["text"] = active_text_map[text_id]
+
         return question_json
